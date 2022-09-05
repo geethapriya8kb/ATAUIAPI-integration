c3de441ea52c31d4b0ad43c79f1c0170e74670b8
Geetha Balakrishna <C-Geetha.Balakrishna@charter.com>
2022-09-05 17:29:26 +0530

    ppv initial filter

--- src/app/agent-os/common/ppv/ppv.component.ts
+++ src/app/agent-os/common/ppv/ppv.component.ts
@@ -1,16 +1,52 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CardDataService } from 'src/app/agent-os/services/card-data.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+export interface Owner{
+  id: string;
+  name: string;
+  dateOfBirth: Date;
+  address: string;
+}
 @Component({
   selector: 'app-ppv',
   templateUrl: './ppv.component.html',
   styleUrls: ['./ppv.component.scss']
 })
-export class PpvComponent implements OnInit {
+
+export class PpvComponent implements OnInit, AfterViewInit {
+  public displayedColumns = ['name', 'dateOfBirth', 'address', 'details', 'update', 'delete'
+];
+  public dataSource = new MatTableDataSource<Owner>();
+  @ViewChild(MatSort) sort: MatSort;
   constructor(private cardDataService:CardDataService, public dialogRef: MatDialogRef<PpvComponent>,
-    @Inject(MAT_DIALOG_DATA) public popUpData: any) { }
+    @Inject(MAT_DIALOG_DATA) public popUpData: any,) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getAllOwners();
   }
-
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+  }
+  public getAllOwners = () => {
+    this.cardDataService.getCardData('api/owner')
+    .subscribe(res => {
+      this.dataSource.data = res as Owner[];
+    })
+  }
+  public redirectToDetails = (id: string) => {
+    
+  }
+  public redirectToUpdate = (id: string) => {
+    
+  }
+  public redirectToDelete = (id: string) => {
+    
+  }
+  public doFilter = (value: string) => {
+    this.dataSource.filter = value.trim().toLocaleLowerCase();
+  }
 }
