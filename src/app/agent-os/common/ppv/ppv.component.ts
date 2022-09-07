2ba9734b0a469c6e34138362e78a648206f5bad7
Geetha Balakrishna <C-Geetha.Balakrishna@charter.com>
2022-09-07 13:53:00 +0530

    ppv changes

--- src/app/agent-os/common/ppv/ppv.component.ts
+++ src/app/agent-os/common/ppv/ppv.component.ts
@@ -19,6 +19,9 @@ export class PpvComponent implements OnInit, AfterViewInit {
   dataSource = new MatTableDataSource<any>();
   columnsToDisplay = [];
   data: any = {};
+  orderedeEvents: any = {};
+  orderedEventsDs = new MatTableDataSource<any>();
+  oeColumnsToDisplay = [];
   @ViewChild(MatSort)
   sort!: MatSort;
   constructor(
@@ -31,28 +34,47 @@ export class PpvComponent implements OnInit, AfterViewInit {
   ngOnInit() {
     const accountNumber = this.searchService.getAccountNumber();
     this.getValuetoTable(accountNumber);
+    this.getValuetoOrderedEvents(accountNumber); //need to create new json for ordered events, need to update db hile adding from ppv and get it back from db
   }
+
   ngAfterViewInit(): void {
     this.searchService.sharedValue$.subscribe((val: any) => {
       const accountNumber = val ? String(val) : '';
-
       this.getValuetoTable(accountNumber);
+      this.getValuetoOrderedEvents(accountNumber);
     });
   }
+
   getValuetoTable(accountNumber) {
     if (!accountNumber || accountNumber === '') accountNumber = 'empty';
-    const path = `${accountNumber}/ts-event-history`;
+    const path = `${accountNumber}/ppv`;
     this.cardDataService.getCardDatafromService(path).subscribe({
       next: (resp) => (this.data = resp),
       error: (err) => console.error(err),
       complete: () => {
-        this.dataSource.data = this.data.eventHistoryTable;
-        this.columnsToDisplay = this.data.eventHistoryColumns;
+        this.dataSource.data = this.data.ppvTable;
+        this.columnsToDisplay = this.data.ppvColumns;
       },
     });
   }
 
+  getValuetoOrderedEvents(accountNumber) {
+    // if (!accountNumber || accountNumber === '') accountNumber = 'empty';
+    // const path = `${accountNumber}/ordered-events`;
+    const path = `empty/ppv`;
+    this.cardDataService.getCardDatafromService(path).subscribe({
+      next: (resp) => (this.orderedeEvents = resp),
+      error: (err) => console.error(err),
+      complete: () => {
+        this.orderedEventsDs.data = this.orderedeEvents.ppvTable;
+        this.oeColumnsToDisplay = this.orderedeEvents.ppvColumns;
+      },
+    });
+  }
+  
   public doFilter = (value: string) => {
     this.dataSource.filter = value.trim().toLocaleLowerCase();
   };
+
+  returnZero = () => 0;
 }
