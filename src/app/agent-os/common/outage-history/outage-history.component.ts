44d4fd6b0ef5830c081b0cd62ca31ce4f0c4ec4b
Geetha Balakrishna <C-Geetha.Balakrishna@charter.com>
2022-10-12 10:51:32 +0530

    outage history

---
+++ src/app/agent-os/common/outage-history/outage-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-outage-history',
+  templateUrl: './outage-history.component.html',
+  styleUrls: ['./outage-history.component.scss']
+})
+export class OutageHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
